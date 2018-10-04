2e2bc2af3148477014c6f9a0497f22d4a20e81af
Moshe Plotkin <plotkinm@newpaltz.edu>
2018-10-04 12:18:25 -0400

    added express

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const math = require('./math');
+const express = require('express');
 
 
-var something = math.add(2, 2);
+const app = express();
 
-console.log("2 + 2 = " + something);
+app.get("/add", function(req, res){
+    var something = math.add(2, 2);
+    res.send({something: something});
+})
+app.use("/", function(req, res, next){
+    res.send( "hello world");
+});
+
+app.listen(3000);
