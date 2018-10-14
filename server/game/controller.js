e6c24e368eec36cc8241a0e61bd074638c4dd4c5
Moshe Plotkin <plotkinm@newpaltz.edu>
2018-10-14 15:10:14 -0400

    deal cards

--- server/game/controller.js
+++ server/game/controller.js
@@ -9,10 +9,18 @@ const app = express.Router();
 app.get("/", function(req, res){
     res.send(game);
 })
+app.get("/captions/:id", function(req, res){
+    res.send(game.players[req.params.id].captions());
+})
+
 app.post('/players', (req, res) => {
     const player = new Player(req.body.name);
     game.players.push(player);
     res.send(player);
+})
+app.post('/picture', (req, res) => {
+    game.flipPicture();
+    res.send(game.picture);
 })
 
 module.exports = app;
--- server/game/model.js
+++ server/game/model.js
@@ -1,22 +1,34 @@
 const captions  = require('./captions');
-const pictures  = require('./pictures');
+const picture_loader  = require('./pictures');
+
+let pictures = [];
+picture_loader().then(x=> 
+    pictures = x
+);
+let iCaptions = 0;
+let iPicture = 0;
 
 class Game {
     constructor(){
         this.picture = "";
         this.players = [];
-        this.playedCaptions = captions;
-        pictures().then(x=> 
-            this.pictures = x
-        );
+        this.playedCaptions = [];
+    }
+
+    flipPicture(){
+        this.picture = pictures[(iPicture++) % pictures.length];
     }
 }
 
 class Player{
     constructor(name){
+        let _Captions = captions.slice(iCaptions, iCaptions += 7);
+
         this.name = name;
-        this.captions = [];
         this.score = 0;
+        this.captions = () => _Captions;
+
+
     }
 }
 
