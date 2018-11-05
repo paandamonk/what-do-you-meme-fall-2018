d8461d2cb66b91e9c71f3798ac2c25646404f7fd
Robert Williams <paandamonk@gmail.com>
2018-11-05 11:30:52 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const game = require('./game/controller');
+
+const app = express();
+
+const port = 80;
+const server = "localhost";
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+  });
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/", express.static(__dirname + "/../dist/"));
+app.use('/game', game);
+
+app.listen(port);
+
+// eslint-disable-next-line no-console
+console.log(`listening on: http://${server}:${port}`);
---
+++ src/views/Game.vue
@@ -0,0 +1,100 @@
+<template>
+<div>
+    <div class="alert alert-success">
+        Yay we have a game!
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card" >
+                    <h5 class="card-header">
+                        Players
+                        <a @click.prevent="login" class="btn btn-sm btn-primary" :class="{disabled: playerId() !== null}">+</a>
+                    </h5>
+                    <ul class="list-group list-group-flush">
+                        <li v-for="p in state.players" :key="p.id"
+                            class="list-group-item">
+                            <img />
+                            <h5>{{p.name}}</h5>
+                            <span class="badge badge-primary badge-pill">{{p.score}}</span>
+                        </li>
+ 
+                    </ul>
+            </div>
+            <div class="card" >
+                <h5 class="card-header">My Captions</h5>
+                <ul class="list-group list-group-flush">
+                    <li v-for="c in myCaptions" :key="c" class="list-group-item">{{c}}</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" >
+                <img class="card-img" :src="state.picture.url" :alt="state.picture.name">
+                <a @click.prevent="flipPicture" class="btn btn-primary">Flip Picture</a>
+
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" >
+                <h5 class="card-header">Played Captions</h5>
+                <ul class="list-group list-group-flush">
+                    <li v-for="c in state.playedCaptions" :key="c.text" class="list-group-item">{{c}}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<style lang="scss">
+    li.list-group-item {
+        display: flex;
+        align-content: center;
+        justify-content: space-between;
+        img {
+            width: 30px; height: 30px;
+            margin-right: 5px;
+        }
+        h5 {
+            flex-grow: 1;
+        }
+    }
+</style>
+
+<script>
+import { GetState, FlipPicture, GetMyCaptions, Login, playerId } from '@/services/api_access';
+
+export default {
+    data: function(){
+        return {
+            state: {
+                picture: "",
+                players: [],
+                playedCaptions: [],
+            },
+            myCaptions: [],
+            //playerId: null
+        }
+    },
+    created: function(){
+        this.refresh();
+    },
+    methods: {
+        refresh(){
+            GetState()
+            .then(x=> this.state = x)
+        },
+        flipPicture: function(){
+            FlipPicture()
+            .then(()=> this.refresh())
+        },
+        login: function() {
+            Login(prompt('What is your name?'))
+            .then(()=>  GetMyCaptions().then(x=> this.myCaptions = x) )
+            .then(()=> this.refresh())
+        },
+        playerId: ()=> playerId
+    }
+}
+</script>
